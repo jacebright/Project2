3a8f03fa892287740f23afd89f714c447b5f785e
jacebright <jacebright@gmail.com>
2025-03-29 20:49:17 -0500

    swagger and error handling update

--- controllers/games.js
+++ controllers/games.js
@@ -1,40 +1,42 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllGames = (req, res) => {
+const getAllGames = async (req, res) => {
     //#swagger.tags=['Games']
-    mongodb
-        .getDatabase()
-        .db()
-        .collection('Games')
-        .find()
-        .toArray((err, games) => {
-            if (err) {
-                res.status(400).json({ message: err});
-            }
+    try {
+        const resut = await mongodb
+            .getDatabase()
+            .db()
+            .collection('Games')
+            .find()
+            .toArray();
+                
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(games);
-        });
+            res.status(200).json(result);
+            } catch (err) {
+                res.status(400).json({message: err});
+            };
+
 };
 
-const getSingleGame = (req, res) => {
+const getSingleGame = async (req, res) => {
     //#swagger.tags=['Games']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid id to perform the function');
     }
     const gameId = new ObjectId(req.params.id);
-    mongodb
+    try {
+    const result = await mongodb
         .getDatabase()
         .db()
         .collection('Games')
         .find({_id: gameId})
-        .toArray((err, game) => {
-            if (err) {
-                res.status(400).json({message: err });
-            }
+        .toArray()
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(game[0]);
-        })
+            res.status(200).json(result[0]);
+        } catch (err) {
+            res.status(400).json({ message: err});
+        }
 };
 
 
--- controllers/players.js
+++ controllers/players.js
@@ -1,40 +1,40 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllPlayers = (req, res) => {
+const getAllPlayers = async (req, res) => {
     //#swagger.tags=['Players']
-    mongodb
-        .getDatabase()
-        .db()
-        .collection('Players')
-        .find()
-        .toArray((err, player) => {
-            if (err) {
-                res.status(400).json({ message: err});
-            } 
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(player);
-        });
+    try {
+        const result = await mongodb
+            .getDatabase()
+            .db()
+            .collection('Players')
+            .find()
+            .toArray();
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(player);
+    } catch (err) {
+            res.status(400).json({ message:err});
+        };
 };
 
-const getSinglePlayer = (req, res) => {
+const getSinglePlayer = async (req, res) => {
     //#swagger.tags=['Players']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid id to perform the function');
     }
     const playerId = new ObjectId(req.params.id);
-    mongodb
-        .getDatabase()
-        .db()
-        .collection('Players')
-        .find({_id: playerId})
-        .toArray((err, player) => {
-            if (err) {
-                res.status(400).json({message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(player[0]);
-        })
+    try {
+        const result = await mongodb
+            .getDatabase()
+            .db()
+            .collection('Players')
+            .find({_id: playerId})
+            .toArray()
+        res.setHeader('Content-Type',   'application/json');
+        res.status(200).json(player[0]);
+    } catch (err) {
+        res.status(400).json({message:err});
+    }
 };
 
 const createPlayer = async (req, res) => {
